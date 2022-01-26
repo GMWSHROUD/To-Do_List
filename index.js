3d293d9d85fcc14216957bbaca3e39f6586b21f5
GMWSHROUD <80972441+GMWSHROUD@users.noreply.github.com>
2022-01-26 21:42:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+ //1.ways to print in javascript
+        // console.log("hello world")
+        //alert("me");
+        //document.write("this is document write")
+        //2.javascript console API
+        console.log("hello world");
+        console.warn("this is a warning");
+        console.error("this an error");
+//3. javasript variables
